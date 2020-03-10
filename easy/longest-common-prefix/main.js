9c56aaeb784f9508114e0b9de5fca2ed564d82af
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-03-11 04:15:26 +0900

    fix indent

--- easy/longest-common-prefix/main.js
+++ easy/longest-common-prefix/main.js
@@ -1,18 +1,18 @@
 var longestCommonPrefix = function(strs) {
-    if(!strs[0]) return "";
-    let res = "";
-    let cur = "";
-    let i = 0;
-    while(i < strs[0].length){
-        cur = strs[0].substring(0, i + 1);
-        let flag = strs.every(x => {
-            return x.startsWith(cur);
-        })
-        if(flag === true){
-            res = cur;
-        }
-        else break;
-        i ++;
+  if(!strs[0]) return "";
+  let res = "";
+  let cur = "";
+  let i = 0;
+  while(i < strs[0].length){
+    cur = strs[0].substring(0, i + 1);
+    let flag = strs.every(x => {
+      return x.startsWith(cur);
+    })
+    if(flag === true){
+      res = cur;
     }
-    return res;
+    else break;
+    i ++;
+  }
+  return res;
 };

59a7af0af7930e82819cf90a790dffdd25cfd683
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-03-18 23:29:07 +0900

    feat: add

---
+++ easy/merge-two-sorted-lists/index.js
@@ -0,0 +1,22 @@
+var mergeTwoLists = function(l1, l2) {
+    let list = new ListNode()
+    let head = list
+    while (l1 !== null && l2 !== null) {
+      if (l1.val < l2.val) {
+        list.next = new ListNode(l1.val)
+        l1 = l1.next
+      } else {
+        list.next = new ListNode(l2.val)
+        l2 = l2.next
+      }
+      list = list.next
+    }
+    if (l1 !== null) {
+      list.next = l1
+    }
+    if (l2 !== null) {
+      list.next = l2
+    }
+    console.log(head.next);
+    return head.next
+};
